26af26a0ac8bb0cc3a1d9033e2b833eb340388f0
fujita_kohei <fujita.kohei04@gmail.com>
2026-03-11 16:49:15 +0900

    feat: add work name and work description on work detail.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,10 @@ import PT1 from "@/app/images/works/PT/PTdef.png";
 import PT2 from "@/app/images/works/PT/PTworking.png";
 import PT3 from "@/app/images/works/PT/PTrestiong.png";
 
+import JIA1 from "@/app/images/works/JIA/J1F_1.png";
+import JIA2 from "@/app/images/works/JIA/J1F_2.png";
+import JIA3 from "@/app/images/works/JIA/J1F_Win.png";
+
 
 type WorkContent = {
   workName: string;
@@ -29,51 +33,51 @@ const WORKS: WorkContent[] = [
     examples: [
       {
         img: GMK1,
-        description: "簡単な例",
+        description: "対戦の様子１",
       },
       {
         img: GMK2,
-        description: "簡単な例",
+        description: "対戦の様子２",
       },
       {
         img: GMK3,
-        description: "簡単な例",
+        description: "対戦の様子３（ライトモード）",
       },
     ],
   },
   {
     workName: "ポモドーロタイマー",
-    workDescription: "ローカル用五目並べ。〇側と✕側のマークに分かれ、盤面上に「縦」「横」「斜め」のいずれかに対し、先にマークを５つ並べたほうが勝ち。Next.jsを用いて実装。",
+    workDescription: "ポモドーロ・テクニックを参考に制作したタイマー。はじめ、タイマーは25分に設定されており、25分経つと短いアラームが鳴り、5分の休憩タイマーが開始される。5分経つとまたアラームが鳴り、また25分に設定されて止まる。Pythonのtkinterを用いて実装。",
     examples: [
       {
         img: PT1,
-        description: "簡単な例",
+        description: "タイマー開始前の様子",
       },
       {
         img: PT2,
-        description: "簡単な例",
+        description: "タイマー開始後の作業中の様子",
       },
       {
         img: PT3,
-        description: "簡単な例",
+        description: "タイマー開始後の休憩中の様子",
       },
     ],
   },
   {
-    workName: "五目並べ",
-    workDescription: "ローカル用五目並べ。〇側と✕側のマークに分かれ、盤面上に「縦」「横」「斜め」のいずれかに対し、先にマークを５つ並べたほうが勝ち。Next.jsを用いて実装。",
+    workName: "トマトマン",
+    workDescription: "パックマンを参考に制作した2Dゲーム。当時の教授のオリジナルキャラ、通称「ハトヤマ」という白いキャラを、プレイヤーが操作するトマトが追いかけ、捕まえたタイムを競う。Javaを用いて実装。ちなみに、オブジェクトはすべて手描きのドット絵である。",
     examples: [
       {
-        img: GMK1,
-        description: "簡単な例",
+        img: JIA1,
+        description: "ゲーム中の様子１",
       },
       {
-        img: GMK2,
-        description: "簡単な例",
+        img: JIA2,
+        description: "ゲーム中の様子２",
       },
       {
-        img: GMK3,
-        description: "簡単な例",
+        img: JIA3,
+        description: "勝利画面",
       },
     ],
   },
@@ -158,12 +162,17 @@ const Work = ({workContents}:{workContents: WorkContent[]}) => {
               <button onClick={() => setIsShowWorkDetail(false)}>✕</button>
             </div>
             <div className={styles.workDetailCard} onClick={(e) => e.stopPropagation()}>
+              <div className={styles.workName}>
+                {`${works[workNum].workName}`}
+              </div>
               <div className={styles.workDescription}>
                 {`${works[workNum].workDescription}`}
               </div>
-              
               <div className={styles.workImageFlame} >
                 <Image src={works[workNum].examples[imgNum].img} alt={works[workNum].examples[imgNum].description} className={styles.workImage} fill style={{ objectFit: 'contain' }}/>
+              </div>
+              <div className={styles.workImageDescription}>
+                {`画像 ${imgNum+1}  : ${works[workNum].examples[imgNum].description}`}
               </div>
               <div className={styles.workChanger} >
                 <button onClick={() => setImgNum((imgNum+(IMG_NUM-1))%IMG_NUM)}>＜</button>
