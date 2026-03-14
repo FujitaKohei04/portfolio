868d1a978d6042612d035ba6e5f067bfe05e3a2d
fujita_kohei <fujita.kohei04@gmail.com>
2026-03-14 17:07:59 +0900

    feat: change number to icon in suggestionBox.

--- src/app/components/SuggestionBox.tsx
+++ src/app/components/SuggestionBox.tsx
@@ -1,16 +1,19 @@
 "use client"
 import { useState } from "react";
-import styles from "./SuggestionBox.module.css";
 
+import styles from "./SuggestionBox.module.css";
+import { Send } from "lucide-react";
 
 export const SuggestionBox = () => {
   const [count, setCount] = useState(0);
   return (
-    <button onClick={() => setCount(count+1)}>
+    <button onClick={(() => window.open("", "_blank"))}>
       <div className={styles.suggestBoxNomal}></div>
       <div className={styles.suggestBox60}></div>
       <div className={styles.suggestBoxMinus60}></div>
-      <div className={styles.suggestBoxNomal}>{count}</div>
+      <div className={styles.suggestBoxNomal}>
+        <Send className={styles.suggestBoxIcon} />
+      </div>
     </button>
   );
 }
