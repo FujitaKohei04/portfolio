c475c0987b0e6516810fcbb46575d3972f0294b4
fujita_kohei <fujita.kohei04@gmail.com>
2026-03-20 16:25:32 +0900

    refactor: define space and font-size in global css.

--- src/app/components/Thumnail/Thumnail.tsx
+++ src/app/components/Thumnail/Thumnail.tsx
@@ -4,16 +4,9 @@ import Styles from "./Thumnail.module.css";
 
 export const Thumbnail = () => {
   return (
-    <div >
-      <div className="flex flex-col justify-center gap-4">
-        {/* <div className={styles.thumbnailTitle}>うぇるかむ</div> */}
-        <DrawLogoSVG />
-        <div>
-          <div className={Styles.thumbnailNameAnimation}>Fujita Kohei - Fukui University Student</div>
-          <div></div>
-        </div>
-        
-      </div>
+    <div className="flex flex-col justify-center gap-4">
+      <DrawLogoSVG />
+      <div className={Styles.thumbnailNameAnimation}>Fujita Kohei - Fukui University Student</div>
     </div>
   );
 }
