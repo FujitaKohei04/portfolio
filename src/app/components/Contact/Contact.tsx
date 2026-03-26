30d4c5de7ec1ea607be32ee964b920ada9f74ad2
fujita_kohei <fujita.kohei04@gmail.com>
2026-03-27 00:18:05 +0900

    feat: add contact icons.

--- src/app/components/Contact/Contact.tsx
+++ src/app/components/Contact/Contact.tsx
@@ -1,13 +1,39 @@
-import { Mail } from "lucide-react";
+import { Mail, X } from "lucide-react";
 import Styles from "./Contact.module.css";
 import { TopicTitle } from "../TopicTitle/TopicTitle";
+import { FiGithub } from "react-icons/fi";
+import { FaXTwitter } from "react-icons/fa6";
+import { SiZenn } from "react-icons/si";
+import { CiMail } from "react-icons/ci";
 
+const LinkButton = ({
+  url, 
+  icon: Icon
+}: {
+  url: string, 
+  icon: React.ElementType
+}) => {
+  return (
+    <button 
+      onClick={() => window.open(url, "_blank")}
+    >
+      <Icon className={Styles.icon} style={{border: "1px solid black", borderRadius: "50%"}}/>
+    </button>
+  );
+}
 
 export const Contact = () => {
   return (
     <div>
       <TopicTitle title="Contact" icon={Mail} />
-      <a href="mailto:fujita.kohei04@gmail.com">fujita.kohei04@gmail.com</a>
+      <div className={Styles.contactIcons}>
+      	<a href="mailto:fujita.kohei04@gmail.com">
+          <CiMail className={Styles.icon} style={{border: "1px solid black", borderRadius: "50%"}}/>
+        </a>
+	      <LinkButton url="https://x.com/FujitaK248" icon={FaXTwitter}/>
+        <LinkButton url="https://github.com/FujitaKohei04" icon={FiGithub}/>
+        <LinkButton url="https://zenn.dev/fuji_842" icon={SiZenn} />
+      </div>
     </div>
   );
 }
